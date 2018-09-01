f67ff0845a61345f675d318d7f17f81587beeffb
shahwal <apple@apples-MBP.lan>
2018-09-01 13:03:57 +0530

    GEGGridView Added

---
+++ FabricSell/Classes/EGGridView.swift
@@ -0,0 +1,21 @@
+//
+//  EGGridView.swift
+//  FabricSell
+//
+//  Created by apple on 01/09/18.
+//
+
+import UIKit
+
+class EGGridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+
+}
